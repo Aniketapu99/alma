5a4416bb4b5298197de33f7abfd02b45d3c858e2
Aniketapu99 <patelaniket1322@gmail.com>
2025-04-17 14:21:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="alma",
-    version="0.1",
+    name='alma',
+    version='0.1',
     packages=find_packages(),
-    install_requires=["numpy", "pandas"],
-    author="Aniketapu99",
-    description="ALMA - Arnaud Legoux Moving Average for pandas",
+    install_requires=['numpy', 'pandas'],
 )
